ba069dbf223624f82746a508f0c61626b41c31dc
IvanIsak2000 <vingving648@gmail.com>
2023-04-03 09:39:31 +0300

    change coder func

--- main.py
+++ main.py
@@ -1,79 +1,25 @@
-text = str(input("Enter text in Russian: ")).lower()
-result = []
-for element in text:
-    if "а" in element:
-        result.append(".-")
-    if "б" in element:
-        result.append("-...")
-    if "в" in element:
-        result.append(".--")
-    if "г" in element:
-        result.append("--.")
-    if "д" in element:
-        result.append("-..")
-    if "е" in element:
-        result.append(".")
-    if "й" in element:
-        result.append(".---")
-    if "ж" in element:
-        result.append("...-")
-    if "з" in element:
-        result.append("--..")
-    if "и" in element:
-        result.append("..")
-    if "к" in element:
-        result.append("-.-")
-    if "л" in element:
-        result.append(".-..")
-    if "м" in element:
-        result.append("--")
-    if "н" in element:
-        result.append("-.")
-    if "о" in element:
-        result.append("---")
-    if "п" in element:
-        result.append(".--.")
-    if "р" in element:
-        result.append(".-.")
-    if "с" in element:
-        result.append("...")
-    if "т" in element:
-        result.append("-")
-    if "у" in element:
-        result.append("..-")
-    if "ф" in element:
-        result.append("..-.")
-    if " " in element:
-        result.append(" ")
-    if "х" in element:
-        result.append("....")
-    if "ц" in element:
-        result.append("-.-.")
-    if "ч" in element:
-        result.append("---.")
-    if "ш" in element:
-        result.append("----")
-    if "щ" in element:
-        result.append("--.-")
-    if "ъ" in element:
-        result.append(".--.-.")
-    if "ы" in element:
-        result.append("-.--")
-    if "ь" in element:
-        result.append("-..-")
-    if "э" in element:
-        result.append("..-..")
-    if "ю" in element:
-        result.append("..--")
-    if "я" in element:
-        result.append(".-.-")
-    if text == "выход":
-        print("ЗАВЕРШЕНИЕ ПРОГРАММЫ")
-        exit()
+CODE = {'A': '.-', 'B': '-...', 'C': '-.-.',
+        'D': '-..', 'E': '.', 'F': '..-.',
+        'G': '--.', 'H': '....', 'I': '..',
+        'J': '.---', 'K': '-.-', 'L': '.-..',
+        'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.',
+        'S': '...', 'T': '-', 'U': '..-',
+        'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..',
+
+        '0': '-----', '1': '.----', '2': '..---',
+        '3': '...--', '4': '....-', '5': '.....',
+        '6': '-....', '7': '--...', '8': '---..',
+        '9': '----.'
+        }
 
 
-print("In Morse code")
+def coder(text):
+
+    return ' '.join(CODE.get(i.upper()) for i in text)
 
 
-print(*result,sep=' ')
-for_exit = input("Press to exit...")
+text = str(input('Write text in English: '))
+print(coder(text))
+for_exit = input('Press to exit...')
